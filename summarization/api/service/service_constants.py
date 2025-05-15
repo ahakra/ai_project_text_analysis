b34ab16cbc2e07416ed8f7b92c655fd1684a47c6
Ahmad Akra <ahakra@vanrise.com>
2025-05-15 10:59:03 +0300

    adding to constants

--- summarization/api/service/service_constants.py
+++ summarization/api/service/service_constants.py
@@ -1,12 +1,27 @@
 from bson import ObjectId
+from datetime import datetime
+
+current_timestamp = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
 
 service_id = ObjectId()
 
 SERVICE_UPDATE_INFO = {
-    "service_id": service_id,
+    "service_id": str(service_id),
     "category": "ai",
     "subcategory": "text_analysis",
     "status": "active",
     "version": "v1.0",
     "metadata": {"key": "static_value"}  
+}
+
+SERVICE_REGISTER_INFO = {
+    "service_id": str(service_id),
+    "service_name": "text_summarization",
+    "category": "ai",
+    "subcategory": "text_analysis",
+    "type": "summarization",
+    "task": "summarize",
+    "version": "v1.0",
+    "status": "active",
+    "metadata": {"last_updated": str(current_timestamp)}
 }
--- summarization/main.py
+++ summarization/main.py
@@ -1,29 +1,13 @@
 
 from api.protobufs.service_registry import ServiceRegistryClient
-from bson import ObjectId
+from api.service.service_constants import SERVICE_REGISTER_INFO
 
-from datetime import datetime
 
 
 if __name__ == "__main__":
     client = ServiceRegistryClient(host="localhost", port=50051)
 
-   
-    service_id = ObjectId()
-    current_timestamp = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
-
-
-    service_info = {
-        "service_id": str(service_id),
-        "service_name": "text_summarization",
-        "category": "ai",
-        "subcategory": "text_analysis",
-        "type": "summarization",
-        "task": "summarize",
-        "version": "v1.0",
-        "status": "active",
-        "metadata": {"last_updated": str(current_timestamp)}
-    }
+    service_info = SERVICE_REGISTER_INFO
     client.register_service(service_info)
 
    
